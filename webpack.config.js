68493b16139eabad3860386e52b103dd270254e6
윤동우 <osydoo>
2022-04-26 23:53:32 +0900

    1~3-2

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: {
+    app: "./main.js",
+  },
+  module: {
+    rules: [{}],
+  },
+  plugins: [],
+  output: {
+    filename: "[name].js",
+    path: "./dist",
+  },
+};
